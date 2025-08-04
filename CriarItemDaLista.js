78a80ea71b8b0d582a4534b54cfbe501efe2ad41
CaueHirai <cauehirai@gmail.com>
2025-08-04 18:34:18 -0300

    Add files via upload

---
+++ CriarItemDaLista.js
@@ -0,0 +1,21 @@
+export const inputItem = document.getElementById("input-item");
+
+export function criarItemDaLista(){
+    if (inputItem.value == ""){
+        alert("Escreve alguma coisa pra submeter né ô cabeção");
+        return;
+    }
+
+    const itemDaLista = document.createElement("li");
+    
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+
+    return itemDaLista;
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import { criarItemDaLista } from "./Scripts/CriarItemDaLista.js";
+
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault(); // Evita perder tudo ao atualizar a página
+
+    const itemDaLista = criarItemDaLista();  
+    listaDeCompras.appendChild(itemDaLista); // Adiciona item criado para lista de compras
+    verificarListaVazia(listaDeCompras);
+    inputItem.value = "";
+})
